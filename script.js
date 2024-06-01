bcba3f15b8bbfdd923813ea26a6fe19b07140d58
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-06-02 01:13:59 +0500

    Updated

---
+++ script.js
@@ -0,0 +1,7 @@
+function press(){
+    var locate = window.location.href
+    // window.location.href = "https://www.google.com/"
+    // window.location.replace("https://www.google.com/");
+    window.location.reload(false); //cache reloding
+    // window.location.reload(true); //server reloding
+}
